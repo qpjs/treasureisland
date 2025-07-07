de326e580eb8a2823ed9765a31e43246a9dede87
Joel Schuster <joel.schuster@quietpond.com>
2025-07-07 08:58:09 -0600

    all

---
+++ convert.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Copy and rename regular map tiles: CellImg_X-Y.jpg ➜ MapImg_X_Y.jpg
+for %%F in (CellImg_*-*.jpg) do (
+    set "filename=%%~nF"
+    set "filename=!filename:CellImg_=!"
+    set "filename=!filename:-=_!"
+    copy /Y "%%F" "MapImg_!filename!.jpg" >nul
+)
+
+REM Copy and rename tradewind tiles: CellTradewindImg_X-Y.jpg ➜ MapImg_TW_X_Y.jpg
+for %%F in (CellTradewindImg_*-*.jpg) do (
+    set "filename=%%~nF"
+    set "filename=!filename:CellTradewindImg_=!"
+    set "filename=!filename:-=_!"
+    copy /Y "%%F" "MapImg_TW_!filename!.jpg" >nul
+)
+
+echo Map image tiles copied and renamed (originals preserved).
+pause
